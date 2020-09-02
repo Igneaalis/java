2a11069dee7ab48eb2251eb60d3ab8f4f65843e1
Igneaalis <Nostaleal.ru@yandex.ru>
2020-09-02 20:09:43 +0300

    Workspace initialization.

---
+++ AU2020/src/Main.java
@@ -0,0 +1,12 @@
+/*
+Чтобы запустить программу:
+1 > cd out/production/AU2020
+    2. Нужно учитывать package: если `package ru;`, то необходимо его включать, например:
+        2.1 > java -cp . ru.Main
+        2.2 > java ru.Main
+*/
+public class Main {
+    public static void main(String[] args) {
+	    System.out.println("Hello world!");
+    }
+}
